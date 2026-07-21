c90eeee8b3f46c3b34dbc99108bd9308de6aceeb
SRIKANTH-7S <budigesrikanth00470@gmail.com>
2026-07-21 20:49:22 +0530

    Add: pow-x-n - submission-0

---
+++ Data Structures & Algorithms/pow-x-n/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double res=1;
+        if(n<0){
+            n=-n;
+            for(int i=1;i<=n;i++){
+            res*=x;
+            }
+            return 1/res;
+        }
+        for(int i=1;i<=n;i++){
+            res*=x;
+        }
+        return res;
+    }
+}
